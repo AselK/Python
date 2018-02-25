2cc97f846c2501156a3230f368a440dc94c1d822
AselK <35496766+AselK@users.noreply.github.com>
2018-02-25 21:14:34 +0300

    update position
    changing the order in which the system asks the users to enter coordinates

--- project1/Task6ChessRook.py
+++ project1/Task6ChessRook.py
@@ -1,8 +1,8 @@
 # Can rook move from first position to the second position
 
 x1 = int(input("Enter x1: "))
-x2 = int(input("Enter x2: "))
 y1 = int(input("Enter y1: "))
+x2 = int(input("Enter x2: "))
 y2 = int(input("Enter y2: "))
 
 if (x1 == x2) or (y1 == y2):
