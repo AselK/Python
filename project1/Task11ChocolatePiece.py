95acdea23a670a8597f1afd942cd4d9aded35401
AselK <asel.nurmuhanbetova@gmail.com>
2018-02-25 23:08:13 +0300

    updating condition

--- project1/Task11ChocolatePiece.py
+++ project1/Task11ChocolatePiece.py
@@ -4,7 +4,8 @@ n = int(input("Enter n: "))
 m = int(input("Enter m: "))
 k = int(input("Enter k: "))
 
-if (k < n * m) and ((k % n == 0) or (m % n == 0)):
+if ((k < n * m) and (not ((k < m) and (k < n)))
+        and ((k % n == 0) or (m % n == 0))):
     print("Yes")
 else:
     print("No")
