25b845d34fdc9104e8b72b75c7db8d94147bf445
AselK <asel.nurmuhanbetova@gmail.com>
2018-02-25 20:35:51 +0300

    adding task 6 chess rook (if)

---
+++ project1/Task6ChessRook.py
@@ -0,0 +1,11 @@
+# Can rook move from first position to the second position
+
+x1 = int(input("Enter x1: "))
+x2 = int(input("Enter x2: "))
+y1 = int(input("Enter y1: "))
+y2 = int(input("Enter y2: "))
+
+if (x1 == x2) or (y1 == y2):
+    print("Yes")
+else:
+    print("No")
