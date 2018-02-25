9f5312fa64e1a46b9a961712a864466d96551fdd
AselK <asel.nurmuhanbetova@gmail.com>
2018-02-25 21:29:32 +0300

    adding task 9 chess queen (if)

---
+++ project1/Task9ChessQueen.py
@@ -0,0 +1,11 @@
+# Can queen move from first position to the second position
+
+x1 = int(input("Enter x1: "))
+y1 = int(input("Enter y1: "))
+x2 = int(input("Enter x2: "))
+y2 = int(input("Enter y2: "))
+
+if (abs(x1 - x2) == abs(y1 - y2)) or (x1 == x2) or (y1 == y2):
+    print("Yes")
+else:
+    print("No")
