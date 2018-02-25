4d44dcae44e7d4574d43e404c368784df8898bda
AselK <asel.nurmuhanbetova@gmail.com>
2018-02-25 21:21:15 +0300

    changing the order in which the system asks the users to enter coordinates

--- project1/Task7ChessKing.py
+++ project1/Task7ChessKing.py
@@ -1,8 +1,8 @@
 # Can king move from first position to the second position
 
 x1 = int(input("Enter x1: "))
-x2 = int(input("Enter x2: "))
 y1 = int(input("Enter y1: "))
+x2 = int(input("Enter x2: "))
 y2 = int(input("Enter y2: "))
 
 if ((x1 == x2 - 1) or (x1 == x2) or (x1 == x2 + 1)) and ((y1 == y2 -1) or (y1 == y2) or (y1 == y2 +1)):
